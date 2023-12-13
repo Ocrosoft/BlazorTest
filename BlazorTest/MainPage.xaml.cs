c36009440ee075cd441edd9f39bb967349be05c1
Ocrosoft <ocrosoft@ocrosoft.com>
2023-12-13 23:20:25 +0800

    commit

---
+++ BlazorTest/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorTest;
+using Microsoft.Maui.Platform;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Loaded += MainPage_Loaded;
+	}
+
+	public Func<string, bool> DragOverCheckPath { get; set; }
+	public Action<IEnumerable<string>> DropCallback { get; set; }
+
+	private void MainPage_Loaded(object sender, EventArgs e)
+	{
+		var view = this.ToPlatform(Handler.MauiContext);
+		MacCatalyst.DragDropHelper.RegisterDragDrop(view, OnDragOver, OnDrop);
+	}
+
+	private bool OnDragOver(string path)
+	{
+		return DragOverCheckPath?.Invoke(path) ?? false;
+	}
+
+	private void OnDrop(IEnumerable<string> paths)
+	{
+		DropCallback?.Invoke(paths);
+	}
+}
---
+++ BlazorTest/Platforms/MacCatalyst/DragDropHelper.cs
@@ -0,0 +1,45 @@
+﻿using Foundation;
+using UIKit;
+
+namespace BlazorTest.MacCatalyst
+{
+	public static class DragDropHelper
+	{
+		public static void RegisterDragDrop(UIView view, Func<string, bool> check, Action<IEnumerable<string>> callback)
+		{
+			var dropInteraction = new UIDropInteraction(new DropInteractionDelegate()
+			{
+				Check = check,
+				Callback = callback,
+			});
+			view.AddInteraction(dropInteraction);
+		}
+	}
+
+	class DropInteractionDelegate : UIDropInteractionDelegate
+	{
+		public Action<IEnumerable<string>> Callback { get; init; }
+		public Func<string, bool> Check { get; init; }
+
+		public override UIDropProposal SessionDidUpdate(UIDropInteraction interaction, IUIDropSession session)
+		{
+			return new UIDropProposal(UIDropOperation.Copy);
+		}
+
+		public override void PerformDrop(UIDropInteraction interaction, IUIDropSession session)
+		{
+			List<string> paths = new List<string>();
+			foreach (var item in session.Items)
+			{
+				item.ItemProvider.LoadItem(UniformTypeIdentifiers.UTTypes.Json.Identifier, null, (data, error) =>
+				{
+					if (data is NSUrl nsData && !string.IsNullOrEmpty(nsData.Path))
+					{
+						paths.Add(nsData.Path);
+					}
+				});
+			}
+			Callback.Invoke(paths);
+		}
+	}
+}
